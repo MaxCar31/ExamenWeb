40464e3cecaf0f9ee46d793772446613b7383b70
Max Carrión <141116497+MaxCar31@users.noreply.github.com>
2024-08-07 18:16:50 -0500

    inicial commit

--- backend/routes/calculator.routes.js
+++ backend/routes/calculator.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const calculatorController = require('../controllers/calculator.controller');
 
-router.get('/add', calculatorController.addition);
+
+router.use(cors())
+
+router.get('add', calculatorController.addition);
 router.get('/subtract', calculatorController.subtraction);
 router.get('/multiply', calculatorController.multiplication);
 router.get('/divide', calculatorController.division);
